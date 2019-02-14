f94c63317158d7abaacc02abb5a850ce473a281b
Oltean-Peter Boroka <erboro97@gmail.com>
2019-02-14 19:34:57 +0200

    survey-frontend-started

--- Modeler/Controllers/SurveyController.cs
+++ Modeler/Controllers/SurveyController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Modeler.Models.DataModels;
 
 namespace Modeler.Controllers
 {
     public class SurveyController : Controller
     {
         // GET: Survey
+        [HttpGet]
         public ActionResult HeartSurvey()
         {
             return View("HeartSurvey");
+        }
+
+        [HttpPost]
+        public ActionResult HeartSurvey(HeartSurveyModel model)
+        {
+
+            return Redirect("/");
         }
     }
 }
---
+++ Modeler/Models/DataModels/HeartSurveyModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modeler.Models.DataModels
+{
+    public class HeartSurveyModel
+    {
+        public string ekg { get; set; }
+        public string age { get; set; }
+        public string riskFactor { get; set; }
+        public string initialTroponin { get; set; }
+        public string cad { get; set; }
+        public string painExercice { get; set; }
+        public string painPalpation { get; set; }
+        public string painCardiac { get; set; }
+    }
+}
