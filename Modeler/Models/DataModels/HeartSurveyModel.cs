de8ff29ea81258b50de23704f7b9ab8bbd660cf9
Oltean-Peter Boroka <erboro97@gmail.com>
2019-02-16 11:16:30 +0200

    frontend-questions-ready

--- Modeler/Models/DataModels/HeartSurveyModel.cs
+++ Modeler/Models/DataModels/HeartSurveyModel.cs
@@ -15,5 +15,62 @@ namespace Modeler.Models.DataModels
         public string painExercice { get; set; }
         public string painPalpation { get; set; }
         public string painCardiac { get; set; }
+        public string stroke { get; set; }
+        public string intubation { get; set; }
+        public string heartRate { get; set; }
+        public string SaSO2 { get; set; }
+        public string heartRateWalkTest { get; set; }
+        public string urea { get; set; }
+        public string CO2 { get; set; }
+        public string troponin { get; set; }
+        public string ntProbnp { get; set; }
+        public string asa { get; set; }
+        public string angina { get; set; }
+        public string cardiacMarker { get; set; }
+        public string breathsMin { get; set; }
+        public string myocardialInfarction { get; set; }
+        public string chf { get; set; }
+        public string peripheral { get; set; }
+        public string cva { get; set; }
+        public string dementia { get; set; }
+        public string copd { get; set; }
+        public string connective { get; set; }
+        public string pepticUlcer { get; set; }
+        public string liver { get; set; }
+        public string diabetes { get; set; }
+        // innentol
+        public string hemiplegia { get; set; }
+        public string CKD { get; set; }
+        public string solidTumor { get; set; }
+        public string leukemia { get; set; }
+        public string lymphoma { get; set; }
+        public string AIDS { get; set; }
+        // idaig
+        public string weightLoss { get; set; }
+        public string myalgia { get; set; }
+        public string headache { get; set; }
+        public string spontaneausAbortion { get; set; }
+        public string seizures { get; set; }
+        public string syncope { get; set; }
+        public string vertigo { get; set; }
+        public string bruits { get; set; }
+        public string pulseInequality { get; set; }
+        public string newLossOfP { get; set; }
+        public string claudication { get; set; }
+        public string carotidodynia { get; set; }
+        public string aorticIncomp { get; set; }
+        public string cardiomyopathy { get; set; }
+        public string smoking { get; set; }
+        public string hemoptysis { get; set; }
+        public string tuberculosis { get; set; }
+        public string alcohol { get; set; }
+        public string fever { get; set; }
+        public string oxygenSaturation { get; set; }
+        public string respiratoryRate { get; set; }
+        public string diminished { get; set; }
+        public string rales { get; set; }
+        public string whoGroup { get; set; }
+        public string renalInsuff { get; set; }
+
     }
 }
