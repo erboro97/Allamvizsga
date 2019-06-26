79f8c44a5505a287d8ef4a0a391e9a8431a8da4c
Oltean-Peter Boroka <erboro97@gmail.com>
2019-06-26 17:34:59 +0300

    pacemaker

--- Modeler/Models/DataModels/DoctorSurveyModel.cs
+++ Modeler/Models/DataModels/DoctorSurveyModel.cs
@@ -10,6 +10,7 @@ namespace Modeler.Models.DataModels
         public string clientId { get; set; }
         public string gender { get; set; }
         public double hr { get; set; }
+        public double pacemaker { get; set; }
         public HeartScore lambdaSurvey { get; set; }
         public double? lambdaValue { get; set; }
         public double v { get; set; }
@@ -28,12 +29,17 @@ namespace Modeler.Models.DataModels
 
         public Client_Survey formatToDatabaseStructure (string userId)
         {
+            bool canChangeLambda = true;
+            if (this.clientId is null)
+            {
+                canChangeLambda = false;
+            }
             getLambdaValueFromSurvey();
             Client_Survey surveyData = new Client_Survey()
             {
                 gender=this.gender,
                 HR=this.hr,
-                lambda=lambdaValue ?? default(double),
+                lambda=(pacemaker==1 && canChangeLambda)? 0.01 : lambdaValue ?? default(double),
                 user_id= (this.clientId is null)? userId : this.clientId,
                 v=this.v,
                 inserted_dtm= DateTime.Now
